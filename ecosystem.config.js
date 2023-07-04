970919dde47954114405c8ce6088f438a98e2029
quannt <qquannguyentrong123@gmail.com>
2023-07-04 19:19:04 +0700

    upload code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'phone-number-next',
     append_env_to_name: true,
-    script: './node_modules/next/dist/bin/next start -p 3003',
+    script: './node_modules/next/dist/bin/next start -p 3000',
     max_memory_restart: '2G'
   }]
 };
