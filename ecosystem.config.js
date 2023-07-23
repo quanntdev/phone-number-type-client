f5ead7f461c5d77e0b9b42344cea7d42279299d0
qrai <78605946+quanntdev@users.noreply.github.com>
2023-07-24 00:12:22 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'phone-number-next',
+    name: 'phone-number-next-3',
     append_env_to_name: true,
     script: './node_modules/next/dist/bin/next start -p 3000',
     max_memory_restart: '2G'
